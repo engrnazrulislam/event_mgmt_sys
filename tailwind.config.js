0049908869039ac4f1e14e8848f46d19768507cb
Mr. Md. Nazrul Islam <57587377+engrnazrulislam@users.noreply.github.com>
2025-07-04 17:16:07 +0600

    tailwind css feature added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html", //tailwind css works on root level templates directory
+    "./**/templates/**/*.html" //tailwind css works on app level templates directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
