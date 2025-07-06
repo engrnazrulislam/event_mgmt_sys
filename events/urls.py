92a6d0752e2d1bc94bf3f3d772594569f7b233d9
Mr. Md. Nazrul Islam <57587377+engrnazrulislam@users.noreply.github.com>
2025-07-06 13:51:05 +0600

    all ui design

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from events.views import dashboard
+from events.views import dashboard, user_dashboard, event_details, events,participants,categories
 
 urlpatterns = [
 path('admin/', admin.site.urls),
-path('dashboard/',dashboard, name='dashboard')
+path('dashboard/',dashboard, name='dashboard'),
+path('user_dashboard/',user_dashboard,name='user_dashboard'),
+path('event_details/',event_details,name='event_details'),
+path('events/',events,name='events'),
+path('participants/',participants,name='participants'),
+path('categories/',categories,name='categories')
 ]
