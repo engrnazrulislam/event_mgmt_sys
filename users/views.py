dab729c8d1991f70bc0f17ed5db2200bc6273457
Mr. Md. Nazrul Islam <57587377+engrnazrulislam@users.noreply.github.com>
2025-07-18 13:00:17 +0600

    create normal sign_up page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from users.forms import RegisterForm
 
 # Create your views here.
+def sign_up(request):
+    if request.method == 'GET': 
+        form = RegisterForm()
+    elif request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request,'registration/registration.html',{'form':form})
